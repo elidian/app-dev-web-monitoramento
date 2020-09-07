f147dcb1d13f4e8f84f7f7a2cd3b6d0b5ec45bce
elidian <elidian89@gmail.com>
2020-09-07 18:37:22 -0300

    template base

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Elidian'}
+    posts = [
+        {
+            'author': {'username': 'cam01'},
+            'body': 'Hey!'
+        },
+        {
+            'author': {'username': 'cam10'},
+            'body': 'Hello!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)

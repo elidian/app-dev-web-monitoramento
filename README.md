b2940bb59f9375d0306644de0d77b5840e21f464
elidian <elidian89@gmail.com>
2020-09-07 22:14:33 -0300

    iniciando projeto de banco de dados e migrações

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ BACKEND DE API DE MONITORAMENTO
 
 tutorial utilizado na construção da api:
 https://blog.miguelgrinberg.com/post/the-flask-mega-tutorial-part-i-hello-world
+
+#venv\Scripts\activate
--- config.py
+++ config.py
@@ -1,5 +1,12 @@
 # configuração de chave secreta
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    # configuração da chave secreta
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Desenvolvimento-Web-2020.1'
+
+    # configuração do Flask-SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False

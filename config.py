d84d72799cba71d4be2b7651cf8fefb04045c7d5
elidian <elidian89@gmail.com>
2020-09-10 11:33:49 -0300

    config update

--- config.py
+++ config.py
@@ -3,10 +3,17 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    print('\nLoad configurations ..\n')
+
+    # active DEBUG
+    DEBUG = True
+
     # configuração da chave secreta
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'Desenvolvimento-Web-2020.1'
 
     # configuração do Flask-SQLAlchemy
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
+    
+    # SQLAlchemy monitorará modificações de objetos
     SQLALCHEMY_TRACK_MODIFICATIONS = False
--- monitoramento.py
+++ monitoramento.py
@@ -6,4 +6,4 @@ def make_shell_context():
     return {'db': db, 'User': User, 'Post': Post, 'Vehicle': Vehicle, 'UserAgent': UserAgent, 'UserCam': UserCam, 'PostAgent': PostAgent, 'PostCam': PostCam}
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run()

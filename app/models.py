d6e118e4063fcc2136d2d6099104e5787216db68
elidian <elidian89@gmail.com>
2020-09-10 10:25:31 -0300

    initial class restructure update

--- app/models.py
+++ app/models.py
@@ -27,6 +27,36 @@ class Post(db.Model):
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
+class Vehicle(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    placa = db.Column(db.String(7), index=True, unique=True)
+    chassi = db.Column(db.String(17), index=True, unique=True)
+    cpf_dono = db.Column(db.String(11))
+    queixa_roubo = db.Column(db.Boolean, index=True)
+    licenciamento = db.Column(db.Boolean, index=True)
+    exercicio = db.Column(db.String, index=True)
+    ipva = db.Column(db.Boolean, index=True)
+
+    def __repr__(self):
+        return '<Vehicle Placa: {}; Chassi: {}; Cpf registrado: {}; Queixa de ROUBO: {}; ipva: {}; Licenciamento: {}; Exercicio: {}>'.format(self.placa, self.chassi, self.cpf_dono, self.queixa_roubo, self.ipva, self.licenciamento, self.exercicio)
+
+    def check_is_placa(self, placa):
+        if placa is not None:
+            if (placa[0].isalpha() and placa[1].isalpha() and placa[2].isalpha() and placa[3].isdecimal() and (placa[4].isdecimal() or placa[4].isalpha()) and placa[5].isdecimal() and placa[6].isdecimal()) is False:
+                return False
+
+class UserCam(User):
+    pass
+
+class UserAgent(User):
+    pass
+
+class PostAgent(Post):
+    pass
+
+class PostCam(Post):
+    pass
+
 @login.user_loader
 def load_user(id):
     return User.query.get(int(id))
